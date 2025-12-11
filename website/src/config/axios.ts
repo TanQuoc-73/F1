c0b4ca37b1b7596261eead554c34c90ccc88517c
TanQuoc-73 <cutorasusu2004@Gmail.com>
2025-12-11 21:13:10 +0700

    be url

--- website/src/config/axios.ts
+++ website/src/config/axios.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://f1-1-gslk.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
+  withCredentials: true,
 });
 
 export default instance; 
