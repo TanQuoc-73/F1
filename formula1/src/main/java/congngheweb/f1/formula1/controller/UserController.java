f7bac0ea37487d0a53838ab600cec04098e60a8a
TanQuoc-73 <cutorasusu2004@Gmail.com>
2025-12-12 17:30:55 +0700

    api

--- formula1/src/main/java/congngheweb/f1/formula1/controller/UserController.java
+++ formula1/src/main/java/congngheweb/f1/formula1/controller/UserController.java
@@ -19,7 +19,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
-@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+@CrossOrigin(origins = "*")
 public class UserController {
 
     @Autowired
